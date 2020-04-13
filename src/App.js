8f3b0e99d0c063b96da38e16d2b82040bbcefdcf
cara <xw33@outlook.com>
2020-04-13 14:19:08 +0100

    use hn api to fetch data

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  const [data, setData] = useState({ hits: [] });
+  const [query, setQuery] = useState("");
+  const [url, setUrl] = useState(
+    "https://hn.algolia.com/api/v1/search?query=hooks"
+  );
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      setError(false);
+      try {
+        const result = await axios(url);
+        setData(result.data);
+      } catch (error) {
+        setError(true);
+      }
+      setLoading(false);
+    };
+    fetchData();
+  }, [url]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Search for HackerNews articles!</h1>
+      <form
+        className="searchbox"
+        onSubmit={(e) => {
+          e.preventDefault();
+          setUrl(`https://hn.algolia.com/api/v1/search?query=${query}`);
+        }}
+      >
+        <input
+          type="text"
+          value={query}
+          placeholder="hooks"
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button type="submit">search</button>
+      </form>
+      <div className="result-container">
+        {loading ? (
+          <div>loading...</div>
+        ) : (
+          <ul>
+            {data.hits.map((item) => (
+              <li key={item.objectID}>
+                <a href={item.url}>{item.title}</a>
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
     </div>
   );
 }
